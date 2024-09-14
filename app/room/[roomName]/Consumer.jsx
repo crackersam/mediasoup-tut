7ef431ef01cbb562ecbd5faaac374e95f143e8a2
Samuel <samames9@icloud.com>
2024-09-14 17:09:57 +0100

    working with expandable video consumer component

--- app/room/[roomName]/Consumer.jsx
+++ app/room/[roomName]/Consumer.jsx
@@ -2,15 +2,15 @@ import React, { useEffect, useRef } from "react";
 
 const Consumer = ({ consumerTransport, socket }) => {
   const videoRef = useRef();
+  const runOnce = useRef(false);
   useEffect(() => {
+    if (runOnce.current) return;
     const { track } = consumerTransport.consumer;
 
     videoRef.current.srcObject = new MediaStream([track]);
 
-    socket.emit("consumer-resume", {
-      serverConsumerId: consumerTransport.serverConsumerTransportId,
-    });
-    console.log(`consumer transport:`, consumerTransport);
+    socket.emit("consumer-resume");
+    runOnce.current = true;
   }, []);
   return <video ref={videoRef} autoPlay controls playsInline />;
 };
